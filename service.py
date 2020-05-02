203ff985a47cc20cd7478cfbf6c73d72143fc912
Israel Abebe <se.israel.abebe@gmail.com>
2020-05-02 02:56:30 +0000

    kind of working~

--- service.py
+++ service.py
@@ -1,17 +1,49 @@
 from flask import Flask
-from predict import Predicter
+from flask import jsonify
 from flask import request,render_template
+import json as json
+from flask_cors import CORS, cross_origin
+from predict import Predicter
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+pred = Predicter()
+
+@app.route('/sentiment',methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def sentiment():
+    if request.method =='POST':
+        sentence = request.form['feedback']
+
+        sentiment = pred.predict_sentiment(sentence)
+
+        if sentiment > 0.5:
+            feeling = 'Good'
+        else:
+            feeling = 'Not Good'
+
+        return render_template('sentiment.html',feedback=feeling)
+
     return render_template('index.html')
 
-@app.route('/sentiment')
-def sentiment_analysis():
-    sentence = request.args.get('sentence')
-    pr = Predicter()
-    return str(pr.predict_sentiment(sentence))
+
+
+
+
+
+# @app.route('/hello',methods=['POST'])
+# def hello():
+#     message = request.get_json(force=True)
+#     name = message['name']
+#     print(name)
+#     response = {
+#         'greeting': 'Hello, ' + name + '!'
+#     }
+#     return jsonify(response)
+
+
 
     
